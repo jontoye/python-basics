0ccd4ea694bc4a5f470cf49e9a8830f0444dc427
Jon <jontoye@gmail.com>
2020-08-31 09:06:07 -0400

    Newest commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-basics
 This is an example project to demonstrate the use of git on Ubuntu.
+
+Make a change here
--- basics.py
+++ basics.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.8
 
 def main(msg):
+    # Another comment
     #Adding a different comment here
     #Print a message
     print(msg)
